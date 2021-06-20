9ff91c0cbfe2f02be0b4e3319e8579feefd8809f
Robert DeBortoli <debortor@oregonstate.edu>
2021-06-20 12:58:01 -0700

    Added function to move sonar around object 360 degrees.

--- scripts/move_sonar.py
+++ scripts/move_sonar.py
@@ -10,6 +10,7 @@ from gazebo_msgs.srv import GetModelState,SetModelState
 from gazebo_msgs.msg import ModelState 
 import tf
 import numpy as np
+import math
 
 # [2.09,1.83,3.59,0,0.507,1.57]
 
@@ -17,7 +18,12 @@ def set_robot_pose(pose):
 	'''
 	Pose is just a list with 6 elements:
 	(x, y, z, roll, pitch, yaw)
+
+	Accounts for tilt of Tritech!!
 	'''
+	pose = list(pose)
+	pose[4] += 0.174533 # to account for tilt of tritech down 10 degrees
+
 	state_msg = ModelState()
 	state_msg.model_name = 'tritech_gemini_720i'
 	state_msg.pose.position.x = pose[0]
@@ -57,6 +63,15 @@ def move_robot_to_pose(current_pose, new_pose, robot_velocity, movements_per_sec
 	# make sure to include rotation diff, o.w. we won't get pure
 	# rotation being recorded
 	rot_weighting = 1. # for weighting rot vs. trans
+
+	# remap to shorter dist so we dont take the long way back around
+	if abs(new_pose[3] - current_pose[3]) > 1.57*2: # 180 degrees
+		current_pose[3] *= -1
+	if abs(new_pose[4] - current_pose[4]) > 1.57*2: 
+		current_pose[4] *= -1
+	if abs(new_pose[5] - current_pose[5]) > 1.57*2: 
+		current_pose[5] *= -1
+
 	rot_diff  = np.array([new_pose[3] - current_pose[3],
 						  new_pose[4] - current_pose[4],
 						  new_pose[5] - current_pose[5]])
@@ -70,14 +85,10 @@ def move_robot_to_pose(current_pose, new_pose, robot_velocity, movements_per_sec
 		set_robot_pose(intermediate_pose)
 		rospy.sleep(1/movements_per_second)
 
-
-
-if __name__ == '__main__':
-	rospy.init_node('set_sonar_pose')
-
-	robot_velocity = 0.1 # m/s
-	movements_per_second = 100 # Hz
-
+def move_custom_traj(robot_velocity, movements_per_second):
+	'''
+	Move to a custom set of poses
+	'''
 	pose_list = [[2.09,1.83,3.59,0,0.507,1.57],
 				 [2.09,1.83,3.8,0,0.507,1.9],
 				 [2.09,1.83,3.3,0,0.507,1.2],
@@ -87,9 +98,46 @@ if __name__ == '__main__':
 				 [2.09,2.3,3.2,0,0.507,1.2]]
 
 	for i in range(1,len(pose_list)):
-		current_pose = pose_list[i-1]
-		current_pose[4] += 0.174533 # to account for tilt of tritech down 10 degrees
-		print('confirm this tilting is necessary')
+		current_pose = pose_list[i-1]		
 		move_robot_to_pose(current_pose, pose_list[i], robot_velocity, movements_per_second)
+
+def move_around_obj(obj_pose, rad_from_obj, robot_velocity, movements_per_second):
+	'''
+	Move 360 degrees around an object
+	'''
+	
+	tilt_angle = 30 * math.pi/180. 
+	rad_sin = rad_from_obj * math.sin(tilt_angle)
+	rad_cos = rad_from_obj * math.cos(tilt_angle)
+	current_pose = [2.09,1.83,3.59,0,tilt_angle,1.57]
+
+	# generate 4 poses, one for each quadrant
+	poses = [[obj_pose[0], obj_pose[1] - rad_cos, obj_pose[2] + rad_sin, 0,tilt_angle,1.57],
+			 [obj_pose[0] - rad_cos, obj_pose[1], obj_pose[2] + rad_sin, 0,tilt_angle,0],
+			 [obj_pose[0], obj_pose[1] + rad_cos, obj_pose[2] + rad_sin, 0,tilt_angle,-1.57],
+			 [obj_pose[0] + rad_cos, obj_pose[1], obj_pose[2] + rad_sin, 0,tilt_angle,-1.57*2],
+			 [obj_pose[0], obj_pose[1] - rad_cos, obj_pose[2] + rad_sin, 0,tilt_angle,1.57]]
+
+	for pose in poses:
+		move_robot_to_pose(current_pose, pose, robot_velocity, movements_per_second)
+		current_pose = pose
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	rospy.init_node('set_sonar_pose')
+
+	robot_velocity = 0.1 # m/s
+	movements_per_second = 100 # Hz
+
+	# move_custom_traj(robot_velocity, movements_per_second)
+
+	obj_pose = [1.97, 3.04, 2.66, 0, 0, 0]
+	rad_from_obj = 2 # radius away from object
+	move_around_obj(obj_pose, rad_from_obj, robot_velocity, movements_per_second)	
 
 
